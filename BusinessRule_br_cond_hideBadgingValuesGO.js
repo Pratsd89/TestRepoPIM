e6c10e206a5641d2387d8893054da15b78102814
Pdutta <Prantik_Dutta@gap.com>
2025-10-01 12:07:52 -0700

    Committed from STEP 2025-10-01T19:07Z

--- BusinessRule_br_cond_hideBadgingValuesGO.js
+++ BusinessRule_br_cond_hideBadgingValuesGO.js
@@ -25,7 +25,7 @@
   "parameters" : [ {
     "id" : "Config",
     "type" : "com.stibo.core.domain.parameter.LOVCrossValidationConfig",
-    "value" : "<map>\n  <entry>\n    <key LOVID=\"lov_badge_flag_type\">DYNAMIC</key>\n    <value>\n      <set>\n        <element LOVID=\"lov_badge_GO\">106</element>\n        <element LOVID=\"lov_badge_GO\">105</element>\n      </set>\n    </value>\n  </entry>\n  <entry>\n    <key LOVID=\"lov_badge_flag_type\">STATIC</key>\n    <value>\n      <set>\n        <element LOVID=\"lov_badge_GO\">107</element>\n      </set>\n    </value>\n  </entry>\n</map>"
+    "value" : "<map>\n  <entry>\n    <key LOVID=\"lov_badge_flag_type\">STATIC</key>\n    <value>\n      <set>\n        <element LOVID=\"lov_badge_GO\">107</element>\n      </set>\n    </value>\n  </entry>\n  <entry>\n    <key LOVID=\"lov_badge_flag_type\">DYNAMIC</key>\n    <value>\n      <set>\n        <element LOVID=\"lov_badge_GO\">106</element>\n        <element LOVID=\"lov_badge_GO\">105</element>\n      </set>\n    </value>\n  </entry>\n</map>"
   }, {
     "id" : "DefiningAttribute",
     "type" : "com.stibo.core.domain.Attribute",
